486b9ffc86509a70226a1edb06ef68108b6b50b5
JcB <jcbagneris@gmail.com>
2007-11-21 21:20:47 +0100

    fonction utilitaire d'envoi de mails
    	new file:   lg/utils.py

---
+++ utils.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+# vim:encoding=utf-8:
+
+import os
+import datetime
+
+from email.MIMEText import MIMEText
+
+SENDMAIL = '/usr/sbin/sendmail'
+
+def send_mail(sender, recipients, subject, msg):
+    """ Sends and email directly through the sendmail command. """
+    for parametre in (sender, recipients, subject, msg):
+        if not parametre:
+            return 255
+    msg = msg.encode('utf8')
+    message = MIMEText(msg,'plain','iso-8859-1')
+    message['From'] = sender
+    message['To'] = ', '.join(recipients)
+    message['Subject'] = subject
+    message['Date'] = datetime.datetime.now().strftime('%c')
+
+    #print message.as_string()
+    #status = 0
+
+    p = os.popen("%s -t" % SENDMAIL, 'w')
+    p.write(message.as_string())
+    status = p.close()
+    return status
+
+if __name__ == "__main__":
+    retour = send_mail('info@learngest.com',['jcbagneris@gmail.com',],'Essai 2', 'Contenu é à')
+    print retour
