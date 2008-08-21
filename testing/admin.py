e653ddd1ffab52eeea48cf4bd9aa77d7f1bbcf9e
JcB <jcbagneris@gmail.com>
2008-08-21 14:57:37 +0200

    Ajout testing_question dans l'admin

--- testing/admin.py
+++ testing/admin.py
@@ -2,11 +2,12 @@
 # vim:encoding=utf-8:
 
 from django.contrib import admin
-from testing.models import Granule, GranuleTitre, Enonce
+from testing.models import Granule, GranuleTitre, Enonce, Question
 
 class GranuleAdmin(admin.ModelAdmin):
     list_display = ('module','rang','slug','nbq','score_min')
-    ordering = ['module','rang']
+    #ordering = ['module','rang']
+    ordering = ('slug',)
 admin.site.register(Granule, GranuleAdmin)
 
 class GranuleTitreAdmin(admin.ModelAdmin):
@@ -18,4 +19,10 @@ class EnonceAdmin(admin.ModelAdmin):
     search_fields = ['libel']
     list_display = ('id','libel')
 admin.site.register(Enonce, EnonceAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['libel']
+    list_display = ('id','libel')
+    list_filter = ('langue','granule')
+admin.site.register(Question, QuestionAdmin)
 
