d30f164bedc59dba9ac3dd3d596e5db8d8c616c0
JcB <jcbagneris@gmail.com>
2010-07-27 16:05:19 +0200

    Meilleure gestion de la langue, par django.utils.translate

--- pages/views.py
+++ pages/views.py
@@ -3,14 +3,22 @@
 
 import os
 import codecs
+import datetime
 
 from django.conf import settings
 from django.shortcuts import render_to_response
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.utils import translation
 
 def page(request, page='home'):
-    lang = request.GET.get('lang',translation.get_language())
+    lang = request.GET.get('lang',None)
+    if lang:
+        request.session['django_language'] = lang
+        translation.activate(lang)
+    else:
+        lang = translation.get_language()
+
+#    lang = request.GET.get('lang',translation.get_language())
     if not page in settings.PAGES:
         page = 'home'
     path = os.path.join(os.path.dirname(settings.PROJECT_PATH),
