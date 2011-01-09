d94c0a7001eb3b434bdc0b5c7b10c3ff67425081
JcB <jcbagneris@gmail.com>
2011-01-09 12:24:48 +0100

    Fix erreur sur date fermeture dans création étudiants introduite par django 1.2

--- settings.py
+++ settings.py
@@ -32,6 +32,7 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'Europe/Paris'
+DATETIME_FORMAT = "Y-m-d H:i:s"
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
